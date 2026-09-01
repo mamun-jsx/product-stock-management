60e9030877d1535e5f0d89beb2aff5e2f5ac1df5
almamun528 <ayapths@gmail.com>
2026-09-01 18:54:25 +0600

    setup tailwindcss as nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
+import "../../global.css";
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "white" },
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
 }
