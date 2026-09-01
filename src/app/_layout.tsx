45a54621af16bbc7445b93c7d7ea5952a38b10f1
almamun528 <ayapths@gmail.com>
2026-09-01 19:15:14 +0600

    add images as assets

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,19 @@
 import "../../global.css";
 
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "white" },
-      }}
-    >
-      <Stack.Screen name="index" />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "white" },
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
